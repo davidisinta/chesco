be6a5710fd863c80081ac37080149436ffea1f8a
David Isinta <david.isinta@gmail.com>
2024-05-20 17:10:23 +0300

    added code that removes the dropdown when user clicks outside

--- chesco-client/src/components/MenuItems.jsx
+++ chesco-client/src/components/MenuItems.jsx
@@ -1,11 +1,27 @@
 import Dropdown from './Dropdown';
-import { useState } from "react";
+import {useEffect, useRef, useState} from "react";
 
 
 const MenuItems = ({ items }) => {
    const [dropdown, setDropdown] = useState(false);
+   useEffect(() => {
+ const handler = (event) => {
+  if (dropdown && ref.current && !ref.current.contains(event.target)) {
+   setDropdown(false);
+  }
+ };
+ document.addEventListener("mousedown", handler);
+ document.addEventListener("touchstart", handler);
+ return () => {
+  // Cleanup the event listener
+  document.removeEventListener("mousedown", handler);
+  document.removeEventListener("touchstart", handler);
+ };
+}, [dropdown]);
+   let ref = useRef();
+
   return (
-    <li className="menu-items">
+    <li className="menu-items" ref={ref}>
       {items.submenu ? (
         <>
           <button type="button" aria-haspopup="menu"
