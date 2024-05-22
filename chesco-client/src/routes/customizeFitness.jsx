2dd2525919d18f3966e2bbdcfd94544e0ce583fd
David Isinta <david.isinta@gmail.com>
2024-05-22 16:53:00 +0300

    more styling

--- chesco-client/src/routes/customizeFitness.jsx
+++ chesco-client/src/routes/customizeFitness.jsx
@@ -5,7 +5,7 @@ const CustomizeFitness = () => {
   return (
 
       <div className="flex h-screen">
-          <div className="w-1/2 bg-black text-white flex items-center justify-center">
+          <div className="w-1/2 bg-image text-white flex items-center justify-center">
           </div>
           <div className="w-3/4 flex items-center justify-center">
               <BasicInfo />
